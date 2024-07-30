4d22a04a50c7b601ad7fff755c8e84ba9fc3593f
Anna Correia <annafgcorreia@gmail.com>
2024-07-29 22:29:13 -0300

    Desafio API Rest (em progresso)

---
+++ API Rest/santander-dev-java/src/main/java/dio/santander_dev_java/SantanderDevJavaApplication.java
@@ -0,0 +1,13 @@
+package dio.santander_dev_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevJavaApplication.class, args);
+	}
+
+}
---
+++ API Rest/santander-dev-java/src/test/java/dio/santander_dev_java/SantanderDevJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.santander_dev_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
